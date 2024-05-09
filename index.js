00f599ca8ef348f96dd0833ca1e6bac73ae12670
Corey_angeles <Warrzone@yandex.ru>
2024-05-10 00:27:17 +0300

    made changes in master

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("Hello world");
 
 // console.log("test");
+
+const userAge = 35;
+
+if (userAge >= 18) {
+  console.log("Вы совершеннолетний");
+} else {
+  console.log("Вы НЕ совершеннолетний");
+}
